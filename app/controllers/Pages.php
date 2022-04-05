3d4ca918b851c5d163c13daf4444a0abec94412b
imran <mlloja.imran@yahoo.com>
2022-04-05 15:34:02 +0200

    Update Full MVC

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,10 +3,10 @@
     class Pages extends Controller {
 
         public function __construct() {
-            $this->postModel = $this->model('Post');
         }
 
         public function index() {
+            
             $data = [
                 'title' => 'Welcome!'
             ];
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -34,4 +34,53 @@
                  echo $this->error;
              }
          }
+
+         // Prepare statement with query
+         public function query($sql) {
+             $this->stmt = $this->dbh->prepare($sql);
+         }
+
+         // Bind values
+         public function bind($param, $value, $type = null) {
+             if(is_null($type)) {
+                switch(gettype($value)) {
+                    case "integer":
+                        $type = PDO::PARAM_INT;
+                        break;
+                    case "boolean":
+                        $type = PDO::PARAM_BOOL;
+                        break;
+                    case "NULL":
+                        $type = PDO::PARAM_NULL;
+                        break;
+                    default:
+                        $type = PDO::PARAM_STR;
+                        break;
+                }
+             }
+
+             $this->stmp->bindValue($param, $value, $type);
+         }
+
+         // Execute the prepared statement
+         public function execute() {
+             return $this->stmt->execute();
+         }
+
+         // Get result set as array of objects
+         public function resultSet() {
+             $this->execute();
+             return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+         }
+
+         public function single() {
+             $this->execute();
+             return $this->stmt->fetch(PDO::FETCH_OBJ);
+         }
+
+         // Get row count
+         public function rowCount() {
+             return $this->stmt->rowCount();
+         }
+
      }
